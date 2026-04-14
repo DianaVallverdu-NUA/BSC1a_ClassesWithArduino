927cb4e2cc62a942311626c39237d60d7604bde8
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2026-04-14 09:41:39 +0100

    Update BSC1a_ClassesWithArduino.ino

--- BSC1a_ClassesWithArduino.ino
+++ BSC1a_ClassesWithArduino.ino
@@ -1,3 +1,10 @@
+#include "pitches.h"
+
+int piezoPin = 7;
+
+int melody[] = {NOTE_E4, NOTE_G4, NOTE_A4, NOTE_B5, NOTE_C5, NOTE_B5, NOTE_A4, NOTE_FS4, NOTE_D4};
+int durations[] = {500, 1000, 500, 750, 250,500, 1000, 500, 750};
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -5,5 +12,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  for(int i = 0; i < sizeof(melody) / sizeof(melody[0]); i++){
+    tone(piezoPin, melody[i], durations[i]);
+    delay(durations[i]);
+  }
 
 }
