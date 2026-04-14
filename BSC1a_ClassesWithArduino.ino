4fa7db851af3655b9f2f3a9856963615c7eae462
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2026-04-14 09:44:40 +0100

    use current note as index

--- BSC1a_ClassesWithArduino.ino
+++ BSC1a_ClassesWithArduino.ino
@@ -5,17 +5,21 @@ int piezoPin = 7;
 int melody[] = {NOTE_E4, NOTE_G4, NOTE_A4, NOTE_B5, NOTE_C5, NOTE_B5, NOTE_A4, NOTE_FS4, NOTE_D4};
 int durations[] = {500, 1000, 500, 750, 250,500, 1000, 500, 750};
 
+int currentNote = 0;
+
 void setup() {
   // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    tone(piezoPin, melody[currentNote], durations[currentNote]);
+    delay(durations[currentNote]);
 
-  for(int i = 0; i < sizeof(melody) / sizeof(melody[0]); i++){
-    tone(piezoPin, melody[i], durations[i]);
-    delay(durations[i]);
-  }
+    currentNote++;
+
+    if(currentNote >= sizeof(melody) / sizeof(melody[0])) {
+      currentNote = 0;
+    }
 
 }
